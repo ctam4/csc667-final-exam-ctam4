98f02da7fbb80b76f524eacaff714120d2bb64fa
Calvin Tam <51683307+ctam4@users.noreply.github.com>
2020-05-20 18:01:00 -0700

    Add GET /refresh, /play, /reset endpoints

--- server.js
+++ server.js
@@ -8,14 +8,48 @@ app.use(cors());
 
 const wss = new WebSocket.Server({ port: 4000 });
 
-let gameState = [];
+let gameState = Array(9).fill(null);
+let winner = null;
+
+app.get('/api/refresh', (req, res) => {
+  res.json({ winner, gameState }).end();
+});
 
 app.get('/api/play', (req, res) => {
-  res.send('');
-})
+  if ('playerId' in req.query && 'position' in req.query) {
+    const { playerId, position } = req.query;
+    if (gameState[position] === null) {
+      gameState[position] = playerId;
+      const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+      ];
+      for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (gameState[a] && gameState[a] === gameState[b] && gameState[a] === gameState[c]) {
+          winner = gameState[a];
+          break;
+        }
+      }
+      res.json({ winner, gameState }).end();
+    } else {
+      res.sendStatus(406).end();
+    }
+  } else {
+    res.sendStatus(400).end();
+  }
+});
 
 app.get('/api/reset', (req, res) => {
-  res.send('');
-})
+  winner = null;
+  gameState = Array(9).fill(null);
+  res.json({ winner, gameState }).end();
+});
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
